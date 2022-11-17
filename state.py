346bf1573ef853083b61a7993b349d45f0bf5898
DylanCavers <s2179041@ed.ac.uk>
2022-11-17 17:52:36 +0000

    testing model with emergency state

--- state.py
+++ state.py
@@ -19,6 +19,9 @@ class StateMachine(object):
                 if transition.message == message and transition.state == self.state:
                     self.state = transition.next_state
                     successful_transitions.append(transition)
+                elif message == "Message E1" or message == "Message E2" or message == "Message E3" or message == "Message E4":
+                    self.state = transition.next_state
+                    successful_transitions.append(transition)
         # print("Transitioned to state: " + str(self.state.name), ", Successful transitions: " + str([st.message for st in successful_transitions]))
         # print()
         return successful_transitions
@@ -29,7 +32,12 @@ class StateMachine(object):
             if transition.state == self.state:
                 messages.append(transition.message)
         if len(messages) != 0:
-            out_message = random.choice(messages)
+            choice_num = random.randint(0,10)
+            if choice_num > 9:
+                out_message = messages[1]
+            else:
+                out_message = messages[0]
+            #out_message = random.choice(messages)
             # print("Sending message: " + str(out_message))
             self.transition([out_message])
             return out_message
@@ -78,20 +86,36 @@ state1 = State("State 1")
 state2 = State("State 2")
 state3 = State("State 3")
 state4 = State("State 4")
+#emergency state
+stateE = State("Emergency")
 
 # Define the transitions
-transition1 = Transition(state1, "Message 3", state3)
-transition2 = Transition(state1, "Message 2", state2)
-transition3 = Transition(state2, "Message 3", state4)
-transition4 = Transition(state3, "Message 2", state4)
+#transition1 = Transition(state1, "Message 3", state3)
+#transition2 = Transition(state1, "Message 2", state2)
+#transition3 = Transition(state2, "Message 3", state4)
+#transition4 = Transition(state3, "Message 2", state4)
+
+#regular transitions
+transition1 = Transition(state1, "Message 1", state2)
+transition2 = Transition(state2, "Message 2", state3)
+transition3 = Transition(state3, "Message 3", state4)
+#transition4 = Transition(state3, "Message 4", state4)
+
+#emergency transitions
+transitionE1 = Transition(state1, "Message E1", stateE)
+transitionE2 = Transition(state2, "Message E2", stateE)
+transitionE3 = Transition(state3, "Message E3", stateE)
+transitionE4 = Transition(state4, "Message E4", stateE)
+
 
 # Define the state machine
-state_machine1 = StateMachine([state1, state2, state3, state4], [transition1, transition2, transition3, transition4])
-state_machine2 = StateMachine([state1, state2, state3, state4], [transition1, transition2, transition3, transition4])
-state_machine3 = StateMachine([state1, state2, state3, state4], [transition1, transition2, transition3, transition4])
-state_machine4 = StateMachine([state1, state2, state3, state4], [transition1, transition2, transition3, transition4])
+state_machine1 = StateMachine([state1, state2, state3, state4, stateE], [transition1, transition2, transition3, transitionE1, transitionE2, transitionE3, transitionE4])
+state_machine2 = StateMachine([state1, state2, state3, state4, stateE], [transition1, transition2, transition3, transitionE1, transitionE2, transitionE3, transitionE4])
+state_machine3 = StateMachine([state1, state2, state3, state4, stateE], [transition1, transition2, transition3, transitionE1, transitionE2, transitionE3, transitionE4])
+state_machine4 = StateMachine([state1, state2, state3, state4, stateE], [transition1, transition2, transition3, transitionE1, transitionE2, transitionE3, transitionE4])
 
-for i in range(1000):
+
+for i in range(3):
     out_messages = []
     for sm in [state_machine1, state_machine2, state_machine3, state_machine4]:
         if random.random() > 0.5:
@@ -108,8 +132,8 @@ for i in range(1000):
         print("State machines are not in the same state")
         break
     print([(i, sm.state.name) for i, sm in enumerate([state_machine1, state_machine2, state_machine3, state_machine4])])
-    for sm in [state_machine1, state_machine2, state_machine3, state_machine4]:
-        sm.reset()
+    #for sm in [state_machine1, state_machine2, state_machine3, state_machine4]:
+    #    sm.reset()
 
 # # Test the state machine (python 3 syntax)
 # print(state_machine)
